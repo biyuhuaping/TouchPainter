25a3b70a40ba5ae149d808b92c376f3ba9069e69
zhoubo <zhoubo@xbniao.com>
2017-08-16 17:14:41 +0800

    创建文件

---
+++ Classes/TouchPainterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TouchPainterAppDelegate.h
+//  TouchPainter
+//
+//  Created by Carlo Chung on 8/21/10.
+//  Copyright 2010 Carlo Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoordinatingController.h"
+
+@interface TouchPainterAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TouchPainter
+//
+//  Created by Carlo Chung on 8/21/10.
+//  Copyright 2010 Carlo Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TouchPainterAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TouchPainterAppDelegate class]));
+    }
+}
